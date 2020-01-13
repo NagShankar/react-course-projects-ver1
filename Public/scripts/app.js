f9c711a70e7b75458619dc3fce433cc08c72ce67
NagShankar <nag.shankar6@gmail.com>
2020-01-13 22:01:27 +0530

    working
    after npm fix

---
+++ Public/scripts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// JSX - JavaScript XML
+var template = React.createElement("p", null, "He won the first ever thailand grand prix");
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-console.log('App.js is running!');
-
 // JSX - JavaScript XML
-var template = <h1>Something new</h1>;
+var template = <p>He won the first ever thailand grand prix</p>;
 
 var appRoot=document.getElementById('app');
 ReactDOM.render(template, appRoot);
